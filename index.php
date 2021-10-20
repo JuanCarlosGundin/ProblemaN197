51db87cdb3e7fbbf3ed33b1d99b70e960c7317e7
LauraFernandez18 <10000068.joan23@fje.edu>
2021-10-20 17:31:54 +0200

    formulario

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Problema 197</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="main">
+        <div>
+            <h2>Problema 197</h2>
+        </div>
+        <form action="operacion.php" method="post">
+        <input type="number" name="string" id="num" placeholder="introduce el texto">
+            <input type="submit" value="enviar">
+        </form>
+        <br/>
+    </div>
+</body>
+
+</html>
